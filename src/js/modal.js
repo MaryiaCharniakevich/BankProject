50d5534a8f7941923d88a1fd3de24faaa5a9618f
MaryiaCh <charniakevichm@gmail.com>
2023-03-07 12:08:42 +0300

    added main

---
+++ src/js/modal.js
@@ -0,0 +1,11 @@
+
+const modal = document.getElementById('#myModal')
+modal.on('show.bs.modal', () => modal.trigger('focus'))
+
+
+
+
+
+// const modal = document.getElementById('#myModal')
+
+// modal.on('shown.bs.modal', () => modal.trigger('focus'))
