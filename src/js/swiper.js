45ebf3ba73cedd37c25fbfa30dfaf3b8af8a86e2
MaryiaCh <charniakevichm@gmail.com>
2023-03-21 15:49:40 +0300

    added two page

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,18 +1,3 @@
-// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
-
- 
-
-//   const swiper = new Swiper('.swiper', {
-//     direction: 'horizontal',
-//     loop: true,
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//   });
-
-//   export default swiper 
-
-
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
 
 const swiper = new Swiper('.swiper', {
